d6eed34319344c204f6501916d00c4bf66a501e6
Thein Naing <theinnaing0908@gmail.com>
2023-05-29 16:37:12 +0800

    update css and Todo.js, AddTodo.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import Title from './components/Title';
-import AddTodo from './components/AddTodo';
-import './App.css';
+
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import Title from "./components/Title";
+import AddTodo from "./components/AddTodo";
+import Todo from "./components/Todo";
+import {
+  collection,
+  query,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const q = query(collection(db, "todos"));
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      let todosArray = [];
+      querySnapshot.forEach((doc) => {
+        todosArray.push({ ...doc.data(), id: doc.id });
+      });
+      setTodos(todosArray);
+    });
+    return () => unsub();
+  }, []);
+
+  const handleEdit = async (todo, title) => {
+    await updateDoc(doc(db, "todos", todo.id), { title: title });
+  };
+  const toggleComplete = async (todo) => {
+    await updateDoc(doc(db, "todos", todo.id), { completed: !todo.completed });
+  };
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, "todos", id));
+  };
   return (
     <div className="App">
       <div>
@@ -11,9 +46,18 @@ function App() {
       <div>
         <AddTodo />
       </div>
-
+      <div className="todo_container">
+        {todos.map((todo) => (
+          <Todo
+            key={todo.id}
+            todo={todo}
+            toggleComplete={toggleComplete}
+            handleDelete={handleDelete}
+            handleEdit={handleEdit}
+          />
+        ))}
+      </div>
     </div>
   );
 }
-
 export default App;
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,43 +1,33 @@
-import React, { useState } from 'react';
-import { db } from '../firebase';
-import { collection, addDoc } from 'firebase/firestore';
+import React from "react";
+import { db } from "../firebase";
+import { collection, addDoc } from "firebase/firestore";
 
-const AddTodo = () => {
-  const [title, setTitle ] = useState('');
-  const handleSubmit= async (e)=> {
+export default function AddTodo() {
+  const [title, setTitle] = React.useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (title !== '') {
-    await addDoc(collection(db, 'todos'), {
-      title: '',
-      completed: false,
-    });
-    setTitle('');
+    if (title !== "") {
+      await addDoc(collection(db, "todos"), {
+        title,
+        completed: false,
+      });
+      setTitle("");
     }
   };
-
   return (
-
-      <form
-      onSubmit={handleSubmit}>
-         <div className="input_container">
-          <input
+    <form onSubmit={handleSubmit}>
+      <div className="input_container">
+        <input
           type="text"
-          placeholder="Enter your todo..."
+          placeholder="Enter todo..."
           value={title}
-          onChange={(e)=> setTitle(e.target.value)}
-          />
-
-      <div className='btn_container'>
-        <button>Save</button>
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
-
-         </div>
-      </form>
-
-
-
-
-  )
+      <div className="btn_container">
+        <button>Add</button>
+      </div>
+    </form>
+  );
 }
-
-export default AddTodo
