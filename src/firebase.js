277df9a559f48b46b900b511822ef3b2bd138c12
Thein Naing <theinnaing0908@gmail.com>
2023-05-29 15:10:13 +0800

    create firebase.js and in this file import functions, Firebase configuration and initialize Firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+
+import { initializeApp} from 'firebase/app';
+import { getFirestore} from 'firebase/firestore';
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyMMBT6bDjSBuUAPH--gLvaJT8yGHtE34",
+  authDomain: "todo-firebase-a1a7a.firebaseapp.com",
+  projectId: "todo-firebase-a1a7a",
+  storageBucket: "todo-firebase-a1a7a.appspot.com",
+  messagingSenderId: "56175329578",
+  appId: "1:56175329578:web:78d8b65cac0344d1605e53"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
